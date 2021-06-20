966531c2a1adc65874530d131bb2b7ea169c72e7
Adam Bollinger <adampbollinger@gmail.com>
2021-06-19 23:00:51 -0400

    Update LoL Match typing from v4 to v5

--- riot_service/src/types/league-of-legends/index.d.ts
+++ riot_service/src/types/league-of-legends/index.d.ts
@@ -18,6 +18,189 @@ declare namespace LOLAPI {
     }
 
     namespace Match {
+        export interface MatchDto {
+            metadata: MetadataDto,
+            info: InfoDto
+        }
+
+        export interface MetadataDto {
+            data_version: string,
+            match_id: string,
+            participants: string[] // A list of participant PUUIDs
+        }        
+    
+        export interface InfoDto {
+            gameCreation: number;
+            gameDuration: number;
+            gameId: number;
+            gameMode: string;
+            gameName: string;
+            gameStartTimestamp: number;
+            gameType: string;
+            gameVersion: string;
+            mapId: number;
+            participants: ParticipantDto[];
+            platformId: string;
+            queueId: number;
+            teams: TeamDto[];
+        }
+
+        export interface ParticipantDto {
+            assists: number;
+            baronKills: number;
+            bountyLevel: number;
+            champExperience: number;
+            champLevel: number;
+            championId: number;
+            championName: string;
+            championTransform: number;
+            consumablesPurchased: number;
+            damageDealtToBuildings: number;
+            damageDealtToObjectives: number;
+            damageDealtToTurrets: number;
+            damageSelfMitigated: number;
+            deaths: number;
+            detectorWardsPlaced: number;
+            doubleKills: number;
+            dragonKills: number;
+            firstBloodAssist: boolean;
+            firstBloodKill: boolean;
+            firstTowerAssist: boolean;
+            firstTowerKill: boolean;
+            gameEndedInEarlySurrender: boolean;
+            gameEndedInSurrender: boolean;
+            goldEarned: number;
+            goldSpent: number;
+            individualPosition: string;
+            inhibitorKills: number;
+            inhibitorsLost: number;
+            item0: number;
+            item1: number;
+            item2: number;
+            item3: number;
+            item4: number;
+            item5: number;
+            item6: number;
+            itemsPurchased: number;
+            killingSprees: number;
+            kills: number;
+            lane: string;
+            largestCriticalStrike: number;
+            largestKillingSpree: number;
+            largestMultiKill: number;
+            longestTimeSpentLiving: number;
+            magicDamageDealt: number;
+            magicDamageDealtToChampions: number;
+            magicDamageTaken: number;
+            neutralMinionsKilled: number;
+            nexusKills: number;
+            nexusLost: number;
+            objectivesStolen: number;
+            objectivesStolenAssists: number;
+            participantId: number;
+            pentaKills: number;
+            perks: PerksDto;
+            physicalDamageDealt: number;
+            physicalDamageDealtToChampions: number;
+            physicalDamageTaken: number;
+            profileIcon: number;
+            puuid: string;
+            quadraKills: number;
+            riotIdName: string;
+            riotIdTagline: string;
+            role: string;
+            sightWardsBoughtInGame: number;
+            spell1Casts: number;
+            spell2Casts: number;
+            spell3Casts: number;
+            spell4Casts: number;
+            summoner1Casts: number;
+            summoner1Id: number;
+            summoner2Casts: number;
+            summoner2Id: number;
+            summonerId: string;
+            summonerLevel: number;
+            summonerName: string;
+            teamEarlySurrendered: boolean;
+            teamId: number;
+            teamPosition: string;
+            timeCCingOthers: number;
+            timePlayed: number;
+            totalDamageDealt: number;
+            totalDamageDealtToChampions: number;
+            totalDamageShieldedOnTeammates: number;
+            totalDamageTaken: number;
+            totalHeal: number;
+            totalHealsOnTeammates: number;
+            totalMinionsKilled: number;
+            totalTimeCCDealt: number;
+            totalTimeSpentDead: number;
+            totalUnitsHealed: number;
+            tripleKills: number;
+            trueDamageDealt: number;
+            trueDamageDealtToChampions: number;
+            trueDamageTaken: number;
+            turretKills: number;
+            turretsLost: number;
+            unrealKills: number;
+            visionScore: number;
+            visionWardsBoughtInGame: number;
+            wardsKilled: number;
+            wardsPlaced: number;
+            win: boolean;
+        }
+    
+        export interface PerksDto {
+            statPerks: StatPerksDto;
+            styles: StyleDto[];
+        }    
+
+        export interface StatPerksDto {
+            defense: number;
+            flex: number;
+            offense: number;
+        }
+        export interface StyleDto {
+            description: string;
+            selections: SelectionDto[];
+            style: number;
+        }
+    
+        export interface SelectionDto {
+            perk: number;
+            var1: number;
+            var2: number;
+            var3: number;
+        }
+    
+        export interface TeamDto {
+            bans: TeamBansDto[];
+            objectives: ObjectiveDto;
+            teamId: number;
+            win: boolean;
+        }        
+
+        interface TeamBansDto {
+            championId: number,
+            pickTurn: number
+        }
+    
+        export interface ObjectiveDto {
+            baron: ObjectiveInfoDto;
+            champion: ObjectiveInfoDto;
+            dragon: ObjectiveInfoDto;
+            inhibitor: ObjectiveInfoDto;
+            riftHerald: ObjectiveInfoDto;
+            tower: ObjectiveInfoDto;
+        }
+    
+        export interface ObjectiveInfoDto {
+            first: boolean;
+            kills: number;
+        }
+    }
+
+    namespace Match_V4 {
         interface MatchDto {
             gameId: number,
             participantIdentities: ParticipantIdentityDto[],
