160effc9e1932696da7645abaf56a94681393790
Adam Bollinger <adampbollinger@gmail.com>
2021-07-16 23:12:47 -0400

    Initial conversions from class based to functional

--- client/src/components/account/Account.js
+++ client/src/components/account/Account.js
@@ -1,22 +1,16 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import CreateAccount from './CreateAccount';
+import React from "react";
+import { Route } from "react-router-dom";
+import CreateAccount from "./CreateAccount";
 
-import Login from './Login';
+import Login from "./Login";
 
-class Account extends Component {
-    static propTypes = {
-    }
-
-    render() {
-        return (
-            <div className="App-body">
-                <Route path="/account/login" component={Login} />
-                <Route path="/account/create" component={CreateAccount} />
-            </div>
-        )
-    }
-}
+const Account = () => {
+  return (
+    <div className="App-body">
+      <Route path="/account/login" component={Login} />
+      <Route path="/account/create" component={CreateAccount} />
+    </div>
+  );
+};
 
 export default Account;
-
