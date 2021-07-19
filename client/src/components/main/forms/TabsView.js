9607dfe1a0615bc6278b8b757830fc3483a07032
Adam Bollinger <adampbollinger@gmail.com>
2021-07-19 17:16:33 -0400

    Converted some forms to hooks

--- client/src/components/main/forms/TabsView.js
+++ client/src/components/main/forms/TabsView.js
@@ -8,36 +8,30 @@ import League from './League.js';
 import Warzone from './Warzone.js';
 import TabView from './TabView.js';
 
-class TabsView extends Component {
-    static propTypes = {
-        prop: PropTypes.object
-    }
+const TabsView = () => {
 
-    render() {
-        return (
-            <React.Fragment>
-                <div className="tab-container">
-                    <Tabs defaultActiveKey="lol">
-                        <Tab eventKey="lol" title="League of Legends">
-                            <TabView
-                                form={
-                                    <League></League>
-                                }
-                            ></TabView>
-                        </Tab>
-                        <Tab eventKey="warzone" title="COD: Warzone">
-                            <TabView
-                                form={
-                                    <Warzone></Warzone>
-                                }
-                            ></TabView>
-                        </Tab>
-                    </Tabs>
-
-                </div>
-            </React.Fragment>
-        )
-    }
+    return (
+        <React.Fragment>
+            <div className="tab-container">
+                <Tabs defaultActiveKey="lol">
+                    <Tab eventKey="lol" title="League of Legends">
+                        <TabView
+                            form={
+                                <League></League>
+                            }
+                        ></TabView>
+                    </Tab>
+                    <Tab eventKey="warzone" title="COD: Warzone">
+                        <TabView
+                            form={
+                                <Warzone></Warzone>
+                            }
+                        ></TabView>
+                    </Tab>
+                </Tabs>
+            </div>
+        </React.Fragment>
+    )
 }
 
 export default TabsView;
