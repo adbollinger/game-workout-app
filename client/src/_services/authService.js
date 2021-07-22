904c948949e026f04a83627491acabe6c82c55d3
Adam Bollinger <adampbollinger@gmail.com>
2021-07-21 22:03:12 -0400

    Update auth port

--- client/src/_services/authService.js
+++ client/src/_services/authService.js
@@ -4,7 +4,7 @@ export const authService = {
     getUser
 };
 
-const baseUrl = 'http://localhost:4000/api/auth';
+const baseUrl = 'http://localhost:4100/api/auth';
 
 function login(name, password) {
     const requestOptions = {
